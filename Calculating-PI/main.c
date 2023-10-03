9bc71ef4902a437dd0e0d85473e7ed970751998c
Borbak820 <erikhaubrich@gmail.com>
2023-10-03 20:44:19 +0200

    Added ideas
    Added ideas to calculate PI

--- Calculating-PI/main.c
+++ Calculating-PI/main.c
@@ -72,3 +72,61 @@ void vLedBlink(void *pvParameters) {
 		vTaskDelay(100 / portTICK_RATE_MS);
 	}
 }
+
+
+/*
+
+Leibniz Folge:
+
+float leibnizPi(float n){
+	double pi=1.0;
+	int i;
+	int N;
+	for (i=3, N=2*n+1; i<=N; i+=2)
+	pi += ((i&2) ? -1.0 : 1.0) / i;
+	return 4*pi;
+}
+
+*/
+
+/*
+
+Nilakantha Folge
+
+?=3+4/(2·3·4)-4/(4·5·6)+4/(6·7·8)-4/(8·9·10)+4/(10·11·12)-4/(12·13·14)
+
+
+// Function to calculate PI
+double calculatePI(double PI, double n,
+double sign)
+{
+	// Add for 1000000 terms
+	for (int i = 0; i <= 1000000; i++) {
+		PI = PI + (sign * (4 / ((n) * (n + 1)
+		* (n + 2))));
+		
+		// Addition and subtraction
+		// of alternate sequences
+		sign = sign * (-1);
+		
+		// Increment by 2 according to formula
+		n += 2;
+	}
+	
+	// Return the value of Pi
+	return PI;
+}
+
+// Driver code
+void main()
+{
+	
+	// Initialise sum=3, n=2, and sign=1
+	double PI = 3, n = 2, sign = 1;
+	
+	// Function call
+	printf("The approximation of Pi is %0.8lf\n",calculatePI(PI, n, sign));
+}
+
+
+*/
